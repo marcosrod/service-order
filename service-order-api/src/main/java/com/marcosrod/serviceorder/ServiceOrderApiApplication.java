461bd71c6c7b628967b94cd766a4d996619820a8
Marcod18 <marcos-rodrigo05@hotmail.com>
2024-07-24 10:57:48 -0300

    Fix: add spring data web support to shut pageimpl warning

--- service-order-api/src/main/java/com/marcosrod/serviceorder/ServiceOrderApiApplication.java
+++ service-order-api/src/main/java/com/marcosrod/serviceorder/ServiceOrderApiApplication.java
@@ -3,7 +3,11 @@ package com.marcosrod.serviceorder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
+
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class})
 public class ServiceOrderApiApplication {
 
